8fd8071180c7a19e9d795c6bac8d47a4e15718ec
fleur-t <fleurthijssen07@gmail.com>
2026-02-24 17:54:33 +0100

    Made the form to change the info for the tasks. You just can't see it yet

--- src/components/taskForm.jsx
+++ src/components/taskForm.jsx
@@ -4,7 +4,16 @@ import '../styles/taskForm.css';
 function TaskForm() {
     return(
         <>
-        
+        <div className="task-form">
+            <h2>Change Task</h2>
+            <form action="">
+                <label htmlFor="task-title">Title:</label>
+                <input type="text" id="task-title" required/>
+                <label htmlFor="task-info">Description:</label>
+                <textarea id="task-info" rows="4" required></textarea>
+                <button className="form-button" type="submit">Save</button>
+            </form>
+        </div>
         </>
     )
 }
