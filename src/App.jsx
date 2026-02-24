c70a523b09bf5e141058e30dacfa14ef6c7b8fee
fleur-t <fleurthijssen07@gmail.com>
2026-02-24 11:40:27 +0100

    added 3 lists to see the status and you can add tasks to the to do list

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,46 @@ import './App.css'
 import Card from './components/card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([])
+
+  const addTask = () => {
+    const newTask = {
+      id: Date.now(),
+      title: 'New Task',
+      Date: new Date().toLocaleDateString(),
+      description: 'Task description'
+    }
+
+    setTasks([...tasks, newTask])
+  }
 
   return (
     <>
       <div className="Header">
         <h1 className='Title'>TaskLens</h1>
+        <div className="add-task">
+        <button className='add-button' onClick={addTask}>Add Task</button>
       </div>
-      <div className="card">
-        <Card />
-        <Card />
-        <Card />
       </div>
+      <div className="board-wrapper">
+        <div className="board-container">
+          <h1 className="board-title">To Do</h1>
+          <div className="card">
+            {tasks.map(task => (
+            <Card key={task.id} task={task} />
+            ))}
+          </div>
+          </div>
+          <div className='board-container'>
+            <h1 className="board-title">In Progress</h1>
+          </div>
+          <div className='board-container'>
+            <h1 className="board-title">Done</h1>
+          </div>
+
+      </div>
+
+
     </>
   )
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,19 +4,18 @@ import '../styles/card.css';
 function Card() {
     return (
         <>
-        <div className="board-wrapper">
+        {/* <div className="board-wrapper">
             <div className="board-container">
-                <h1 className="board-title">To Do</h1>
-
+                <h1 className="board-title">To Do</h1> */}
                 <div className="task">
                     <div className="task-header">
                             <h2>Title</h2>
-                            <p>24-02-2026</p>
+                            <p>Date</p>
                     </div>
                 <p>Discription</p>
                 </div>
-            </div>
-        </div>
+            {/* </div>
+        </div> */}
         </>
     )
 }
